5eb9d729e19f0cb963f2105394d3b6d80a21f2f0
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-02-20 19:43:03 -0500

    updated all

--- makeMoney.js
+++ makeMoney.js
@@ -1,48 +1,28 @@
 
-(function() {
-
-    const makeMoneyButton = document.getElementById('button');
-    // console.log(makeMoneyButton);
-    makeMoneyButton.addEventListener('click', () => {
-        const totalCoins = document.getElementById('howManyInput').value;
-            console.log(totalCoins);
-        let whichCoinsInput = document.getElementById('whichCoinsSelection').value;
-        // console.log(whichCoinsInput);
-            if (whichCoinsInput === 'penny') {
-                const pennies = document.getElementById('penny').value;
-                // console.log(pennies);
-                    document.getElementById('pennyCircle').style.visibility = "visible";
-                    document.getElementById('dimeCircle').style.visibility = "hidden";
-                    document.getElementById('nickelCircle').style.visibility = "hidden";
-                    document.getElementById('quarterCircle').style.visibility = "hidden";
-                
-            } else if (whichCoinsInput === 'nickel') {
-                const nickels = document.getElementById('nickel').value;
-                // console.log(nickels);
-                    document.getElementById('pennyCircle').style.visibility = "hidden";
-                    document.getElementById('dimeCircle').style.visibility = "hidden";
-                    document.getElementById('nickelCircle').style.visibility = "visible";
-                    document.getElementById('quarterCircle').style.visibility = "hidden";
-            } else if (whichCoinsInput === 'dime') {
-                const dimes = document.getElementById('dime').value;
-                // console.log(dimes);
-                    document.getElementById('pennyCircle').style.visibility = "hidden";
-                    document.getElementById('dimeCircle').style.visibility = "visible";
-                    document.getElementById('nickelCircle').style.visibility = "hidden";
-                    document.getElementById('quarterCircle').style.visibility = "hidden";
-            } else if (whichCoinsInput === 'quarter') { 
-                const quarters = document.getElementById('quarter').value;
-                // console.log(quarters);
-                    document.getElementById('pennyCircle').style.visibility = "hidden";
-                    document.getElementById('dimeCircle').style.visibility = "hidden";
-                    document.getElementById('nickelCircle').style.visibility = "hidden";
-                    document.getElementById('quarterCircle').style.visibility = "visible";
-            } else {
-                // alert("Invalid selection!  Please choose a coin type."); <== uncomment this!!
-            }
-                //The above logs as it should - right now.
-    
-    })
-    
-    
-    }())
+(() => {
+    const howManyInput = document.getElementById('number');
+    const coinType = document.getElementById('coinType');
+    const makeMoneyButton = document.getElementById('makeMoneyButton');
+    const coinContainer = document.getElementById('coin-container');
+function makeMoney(event) {
+    event.preventDefault();
+    const howManyInputValue = +howManyInput.value;
+        for (let i = 0; i <howManyInputValue; i++){
+            const newCoin = document.createElement('div');
+            const newCoinP = document.createElement('p');
+            newCoinP.innerText = coinType.value
+            newCoin.append(newCoinP);
+            newCoin.addEventListener('click', (event) => {
+                if (event.target.classList.contains('coin')) {
+                    event.target.remove();
+                }else {
+                    event.target.parentNode.remove();
+                }
+            });
+            newCoin.classList.add('coin');
+            newCoin.classList.add(coinType.value);
+            coinContainer.append(newCoin);
+        }
+};
+makeMoneyButton.addEventListener('click', makeMoney)
+})();
